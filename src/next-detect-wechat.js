2f2665cbc5dbba7352edf21742c2fe63d961a59a
afeiship <1290657123@qq.com>
2018-01-12 13:51:32 +0800

    issue: opmtize

--- src/next-detect-wechat.js
+++ src/next-detect-wechat.js
@@ -3,13 +3,12 @@
   var global = global || this || self || window;
   var nx = global.nx || require('next-js-core2');
   var USER_AGENT = global.navigator.userAgent;
-  var WECHAT_RE = /MicroMessenger/i;
-  var MICROMESSENGER = 'micromessenger';
+  var MICROMESSENGER = 'MicroMessenger';
 
   var NxDetectWechat = nx.declare('nx.DetectWechat', {
     statics: {
       isWechat: function(){
-        return USER_AGENT.indexOf('MicroMessenger') > -1;
+        return USER_AGENT.indexOf(MICROMESSENGER) > -1;
       }
     }
   });
