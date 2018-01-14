725fa62ca080a906272985a14bb794e2b2f5defd
afeiship <1290657123@qq.com>
2018-01-14 10:48:28 +0800

    issue: version for wehat

--- src/next-detect-wechat.js
+++ src/next-detect-wechat.js
@@ -3,12 +3,17 @@
   var global = global || this || self || window;
   var nx = global.nx || require('next-js-core2');
   var USER_AGENT = global.navigator.userAgent;
+  var VERSION_RE = /MicroMessenger\/([\d\.]+)/i;
   var MICROMESSENGER = 'MicroMessenger';
+  var EMPTY_MATCH = [,0];
 
   var NxDetectWechat = nx.declare('nx.DetectWechat', {
     statics: {
       isWechat: function(){
         return USER_AGENT.indexOf(MICROMESSENGER) > -1;
+      },
+      version: function(){
+        return ( USER_AGENT.match(VERSION_RE) || EMPTY_MATCH )[1];
       }
     }
   });
